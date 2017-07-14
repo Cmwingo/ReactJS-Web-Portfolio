7f0e532d2713093e2461735f56e4173d2beab5a1
Wingo <cmwingo@gmail.com>
2017-07-14 14:19:45 -0700

    Add react-moment and display today's date in correct format

--- src/App.js
+++ src/App.js
@@ -2,20 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import Moment from 'react-moment';
+
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      numberInfo: []
+      numberInfo: [],
+      date: new Date()
     };
   }
   componentDidMount() {
     axios.get('http://numbersapi.com/3/trivia')
       .then(res => {
         const numberInfo = res;
-        console.log(res);
         this.setState({ numberInfo });
       });
   }
@@ -29,6 +31,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p style={{display: 'inline'}}>Today''s date is </p><Moment format="MMMM, DD YYYY">{this.state.date}</Moment>
         <div id="numberInfo">
           <p>{this.state.numberInfo.data}</p>
         </div>
